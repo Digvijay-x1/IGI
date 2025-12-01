2b96b3ea7cf3a8be6db38920fc508d0e56be399f
Digvijay-x1 <hackerearthx1@gmail.com>
2025-12-01 22:25:03 +0530

    added search_controller.rb and dockerfile in rails and Orchestration

---
+++ rails/app/controllers/search_controller.rb
@@ -0,0 +1,12 @@
+class SearchController < ApplicationController
+  def index
+    @query = params[:q]
+    @results = []
+
+    if @query.present?
+      # "ranker_service" is the DNS name defined in docker-compose
+      response = HTTParty.get("http://ranker_service:5000/search?q=#{@query}")
+      @results = JSON.parse(response.body)['results']
+    end
+  end
+end
